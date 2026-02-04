4888bbfaa794a36c42e50573c4367546a929e451
mavrix21 <150045973+AdityaRane21@users.noreply.github.com>
2026-02-04 16:11:07 +0530

    new files

---
+++ patterns/Stack/Monotonic Stack/nextGreaterElement2.cpp
@@ -0,0 +1,29 @@
+// circular array nge 
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& arr) {
+
+        int n = arr.size();
+        vector<int> ans(n, -1);
+        stack<int> st;
+
+        for(int i = 2*n - 1; i >= 0; i--) {
+
+            int idx = i % n;
+
+            while(!st.empty() && st.top() <= arr[idx]) {
+                st.pop();
+            }
+
+            if(!st.empty()) {
+                ans[idx] = st.top();
+            }
+
+            st.push(arr[idx]);
+        }
+
+        return ans;
+    }
+};
+
+// Reference - https://www.youtube.com/watch?v=7PrncD7v9YQ
